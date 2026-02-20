fbe8fda148651cb56dd52b99ef1118a5239fd85a
Shishir Poudel <shishir.poudel@trojans.dsu.edu>
2026-02-20 00:04:02 -0600

    pending delegation added

--- src/test-db-wallet.ts
+++ src/test-db-wallet.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createAndStoreWallet } from './chain';
 
 async function test() {
-  const user = await createAndStoreWallet('987654321');
+  const user = await createAndStoreWallet('987654321', 'pending_delegation');
   console.log('✅ Wallet stored:', user);
 }
 
